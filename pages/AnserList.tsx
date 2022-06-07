141cd7aaf622ad7b7a4ef91e823ecdc60fc11256
yamazakiTasuku <yamazaki4193161411@gmail.com>
2022-06-07 10:55:46 +0900

    配列からコンポーネントにメッセージとして表示

--- pages/AnserList.tsx
+++ pages/AnserList.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const AnserList = () => {
+const AnserList = ({anserList}) => { 
+  console.log(anserList);
+  const AnserLists={
+
+  }
+
+
   return (
-    <div>TodoList</div>
+    <div>
+      <h1>{anserList.Netflix}</h1>
+      <h1>{anserList.UNext}</h1>
+      <h1>{anserList.Disney_plus}</h1>
+    </div>
   )
 }
 
--- pages/product/Question.tsx
+++ pages/product/Question.tsx
@@ -4,10 +4,25 @@ import {useRouter} from "next/router"
 
 const App = () => {
     const [ansers,setAnsers] = useState();
+    type anser = {
+        Netflix: string;
+        UNext: string;
+        Disney_plus:string;
+    }
+    const anserLists:anser[] = new Array();
+    anserLists[0] = {
+        Netflix: "a",
+        UNext: "b",
+        Disney_plus:"c",
+    }
+
+
+
 
     return(
         <div>
-            <h1>kaoa</h1>
+            <AnserList anserList={anserLists[0]}/>
+            
         </div>
     )
 
