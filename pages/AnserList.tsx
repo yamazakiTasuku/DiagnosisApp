162b730cfa9318d2ea7d58a163e9ffeeaac67f3f
yamazakiTasuku <yamazaki4193161411@gmail.com>
2022-06-07 12:00:44 +0900

    ボタンを作成

--- pages/AnserList.tsx
+++ pages/AnserList.tsx
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const AnserList = ({anserList}) => { 
-  console.log(anserList);
-  const AnserLists={
-
+const AnserList = ({anserList,anserConter}) => { 
+  type anserConterType={
+    NetflixNum:number;
+    UNextNum:number;
+    DisneyPlus:number;
   }
+  const [anser,setAnsers] = useState<anserConterType[]>([]);
+
+const handleSubmit = (e,anserConter:anserConterType) =>{
+  
+  e.preventDefault();
+  let newAnserConter :anserConterType={
+    NetflixNum : anserConter.NetflixNum + 1,
+    UNextNum : anserConter.UNextNum + 1,
+    DisneyPlus:anserConter.DisneyPlus + 1,
+  }
+  setAnsers([newAnserConter,...anserConter]);
+  console.log(newAnserConter)
+
+}
 
 
   return (
     <div>
-      <h1>{anserList.Netflix}</h1>
-      <h1>{anserList.UNext}</h1>
-      <h1>{anserList.Disney_plus}</h1>
+      
+      <div className='flex justify-center'>
+        <button className=" shadow-lg my-10 mx-10 px-10 py-1  bg-blue-400 text-lg text-white font-semibold rounded  hover:bg-blue-500 hover:shadow-sm hover:translate-y-0.5 transform transition " onClick={(e) => handleSubmit(e,anserConter)}>{anserList.Netflix}</button>
+        <button className=" shadow-lg my-10 mx-10 px-10 py-1  bg-blue-400 text-lg text-white font-semibold rounded  hover:bg-blue-500 hover:shadow-sm hover:translate-y-0.5 transform transition ">{anserList.UNext}</button>
+        <button className=" shadow-lg my-10 mx-10 px-10 py-1  bg-blue-400 text-lg text-white font-semibold rounded  hover:bg-blue-500 hover:shadow-sm hover:translate-y-0.5 transform transition ">{anserList.Disney_plus}</button>
+      
+      </div>
     </div>
   )
 }
--- pages/product/Question.tsx
+++ pages/product/Question.tsx
@@ -4,11 +4,26 @@ import {useRouter} from "next/router"
 
 const App = () => {
     const [ansers,setAnsers] = useState();
+
     type anser = {
         Netflix: string;
         UNext: string;
         Disney_plus:string;
     }
+    type anserConterType={
+        NetflixNum:number;
+        UNextNum:number;
+        DisneyPlus:number;
+    }
+
+    //答えの種類を足していく
+    let anserConter:anserConterType = {
+        NetflixNum : 0,
+        UNextNum : 0,
+        DisneyPlus:0
+    };
+
+    //回答の種類を定義
     const anserLists:anser[] = new Array();
     anserLists[0] = {
         Netflix: "a",
@@ -16,13 +31,26 @@ const App = () => {
         Disney_plus:"c",
     }
 
+    anserLists[1] = {
+        Netflix: "d",
+        UNext: "e",
+        Disney_plus:"f",
+    }
+
+    anserLists[2] = {
+        Netflix: "g",
+        UNext: "h",
+        Disney_plus:"i",
+    }
+
 
 
 
     return(
         <div>
-            <AnserList anserList={anserLists[0]}/>
-            
+            <AnserList anserList={anserLists[0]} anserConter={anserConter}/>
+            <AnserList anserList={anserLists[1]} anserConter={anserConter}/>
+            <AnserList anserList={anserLists[2]} anserConter={anserConter}/>
         </div>
     )
 
